40cdbcbe81a7f4e4ade9971b4ff79ce9648dfedb
thanh-phongn <thanh-phongn@system-exe.com.vn>
2024-03-01 14:41:10 +0700

    fix routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
@@ -26,9 +25,3 @@ export const routes: Routes = [
   },
   { path: '**', component: NotFoundComponent },
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -17,11 +17,12 @@ import { RouterModule } from '@angular/router';
         <p class="mb-8">
           But dont worry, you can find plenty of other things on our homepage.
         </p>
-
+        <div></div>
         <button
+          [routerLink]="['/']"
           class="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700"
         >
-          <a [routerLink]="['/']"> back to homepage </a>
+          back to home page
         </button>
       </div>
       <div class="max-w-lg">
